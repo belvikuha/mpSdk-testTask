631d5b3170c82dd535408dd8954697c39d8edd14
belvikuha <belviktory0.0@gmail.com>
2025-03-20 23:47:53 +0200

    add Tag

--- src/app/matterport.tsx
+++ src/app/matterport.tsx
@@ -1,18 +1,19 @@
 "use client";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
-// import {MpSdk} from "@matterport/sdk";
+import {MpSdk} from "@matterport/sdk";
 
 export default function MatterportPage() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  // const [mpSdk, setMpSdk] = useState();
+  const [mpSdk, setMpSdk] = useState<MpSdk | null>(null);
 
   const onLoad = async () => {
+    if (!iframeRef.current) return;
     let mpSdk;
     const showcaseWindow = iframeRef?.current?.contentWindow;
     try {
       mpSdk = await showcaseWindow?.MP_SDK.connect(showcaseWindow);
-      // setMpSdk(mpSdk);
+      setMpSdk(mpSdk);
     } catch (e) {
       console.error(e);
       return;
@@ -23,6 +24,26 @@ export default function MatterportPage() {
   useEffect(() => {
     onLoad();
   }, []);
+
+  useEffect(() => {
+    if (!mpSdk) return;
+
+    const addTag = async () => {
+      try {
+        const tag = await mpSdk.Tag.add({
+          label: "Office",
+          anchorPosition: {x: 0, y: 1.5, z: 0},
+          stemVector: {x: 0, y: 0.3, z: 0}, // Стебель тега
+          color: {r: 1.0, g: 0.0, b: 0.0},
+        });
+        console.log("Тег добавлен:", tag);
+      } catch (err) {
+        console.error("Ошибка при добавлении тега", err);
+      }
+    };
+
+    addTag();
+  }, [mpSdk]);
 
   return (
     <div style={{width: "100%", height: "100vh"}} className="tyty">
